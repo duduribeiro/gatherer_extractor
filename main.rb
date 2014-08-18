9d1bc7b0861ff956ad3816752e462844ff0999ff
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2014-08-18 10:59:02 -0300

    Use thread to extract cards concurrently from all sets

--- main.rb
+++ main.rb
@@ -1,5 +1,18 @@
 require './gatherer_extractor'
+require './database'
+require './models/magic_set.rb'
+require './models/magic_card.rb'
+require 'pry'
+
 gatherer_extractor = GathererExtractor.new
+Database.new.setup
+start = Time.new
+pool = []
 gatherer_extractor.all_sets.each do |set|
-  puts set
+  set = MagicSet.first_or_create(name: set)
+  pool << Thread.new {
+    gatherer_extractor.all_cards_by_set(set).map{|card| puts card}
+  }
 end
+pool.each(&:join)
+puts "Time: #{Time.new - start}s"
